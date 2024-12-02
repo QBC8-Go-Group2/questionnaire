2c45bb53f2771b8efc55c0634daa1372472884c0
mojtabamovahedi <mojtabamovahedi.eng@gmail.com>
2024-12-02 21:13:01 +0330

    add user service

---
+++ internal/user/domain/user.go
@@ -0,0 +1,24 @@
+package domain
+
+import "time"
+
+type (
+	RoleType uint8
+	UserDbID uint
+	UserID   uint
+)
+
+const (
+	AdminRole RoleType = iota
+	UserRole
+)
+
+type User struct {
+	ID        UserDbID
+	CreatedAT time.Time
+	UserID    UserID
+	Email     string
+	Password  string
+	NatId     string
+	Role      RoleType
+}
---
+++ internal/user/service.go
@@ -0,0 +1,41 @@
+package user
+
+import (
+	"context"
+	"github.com/QBC8-Go-Group2/questionnaire/internal/user/domain"
+	"github.com/QBC8-Go-Group2/questionnaire/internal/user/port"
+)
+
+type service struct {
+	repo port.Repo
+}
+
+func (s *service) CreateUser(ctx context.Context, user domain.User) (domain.UserDbID, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) UpdateUser(ctx context.Context, user domain.User) (domain.UserDbID, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) FindUserWithUserID(ctx context.Context, userId domain.UserID) (domain.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) FindUserWithUserDbID(ctx context.Context, userDbId domain.UserDbID) (domain.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) DeleteUserWithUserID(ctx context.Context, user domain.UserID) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *service) DeleteUserWithUserDbId(ctx context.Context, user domain.UserDbID) error {
+	//TODO implement me
+	panic("implement me")
+}
