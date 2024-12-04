45b1b07e23e10a32fdcf38952abc2823c762afcc
amir <samirhashemi@protonmail.com>
2024-12-04 20:52:08 +0330

    Add application setup and server init

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/QBC8-Go-Group2/questionnaire/api/handler/http"
+	"github.com/QBC8-Go-Group2/questionnaire/app"
+	"github.com/QBC8-Go-Group2/questionnaire/config"
+	"github.com/QBC8-Go-Group2/questionnaire/internal/auth"
+	"github.com/QBC8-Go-Group2/questionnaire/internal/user"
+	"github.com/QBC8-Go-Group2/questionnaire/pkg/adapter/storage"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("yo")
+	cfg := config.MustReadConfig("config.json")
+	application := app.MustNewApp(cfg)
+
+	userRepo := storage.NewUserRepo(application.DB())
+	userService := user.NewService(userRepo)
+	otpStore := storage.NewOTPStore(application.Redis())
+	authService := auth.NewService(userService, otpStore)
+	authHandler := http.NewAuthHandler(authService)
+
+	app := fiber.New()
+	http.RegisterAuthRoutes(app, authHandler)
+
+	log.Fatal(app.Listen(":3000"))
 }
