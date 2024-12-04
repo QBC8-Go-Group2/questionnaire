00c1636b78933c2ad13bb1790d349d3fad6cfc23
amir <samirhashemi@protonmail.com>
2024-12-04 20:22:13 +0330

    implement user service methods

--- internal/user/service.go
+++ internal/user/service.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"context"
+
 	"github.com/QBC8-Go-Group2/questionnaire/internal/user/domain"
 	"github.com/QBC8-Go-Group2/questionnaire/internal/user/port"
 )
@@ -15,31 +16,25 @@ func NewService(repo port.Repo) port.Service {
 }
 
 func (s *service) CreateUser(ctx context.Context, user domain.User) (domain.UserDbID, error) {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.Create(ctx, user)
 }
 
 func (s *service) UpdateUser(ctx context.Context, user domain.User) (domain.UserDbID, error) {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.Update(ctx, user)
 }
 
 func (s *service) FindUserWithUserID(ctx context.Context, userId domain.UserID) (domain.User, error) {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.FindWithUserID(ctx, userId)
 }
 
 func (s *service) FindUserWithUserDbID(ctx context.Context, userDbId domain.UserDbID) (domain.User, error) {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.FindWithUserDbID(ctx, userDbId)
 }
 
 func (s *service) DeleteUserWithUserID(ctx context.Context, user domain.UserID) error {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.DeleteWithUserID(ctx, user)
 }
 
 func (s *service) DeleteUserWithUserDbId(ctx context.Context, user domain.UserDbID) error {
-	//TODO implement me
-	panic("implement me")
+	return s.repo.DeleteWithUserDbId(ctx, user)
 }
