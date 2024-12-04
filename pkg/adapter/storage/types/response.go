0eba0ab95d5871e93333f0543f5601087094754f
mojtabamovahedi <mojtabamovahedi.eng@gmail.com>
2024-12-04 20:15:13 +0330

    write test for response repo

--- pkg/adapter/storage/types/response.go
+++ pkg/adapter/storage/types/response.go
@@ -3,11 +3,11 @@ package types
 import "time"
 
 type Response struct {
-	ID         uint      `gorm:"primary_key;auto_increment;unique"`
-	Type       uint8     `gorm:"not null"`
-	UserID     uint      `gorm:"not null"`
-	QuestionID uint      `gorm:"not null"`
-	Data       string    `gorm:"not null"`
-	OptionID   uint      `gorm:"not null"`
-	CreatedAt  time.Time `gorm:"not null"`
+	ID         uint  `gorm:"primary_key;auto_increment;unique"`
+	Type       uint8 `gorm:"not null"`
+	UserID     uint  `gorm:"not null"`
+	QuestionID uint  `gorm:"not null"`
+	Data       string
+	OptionID   uint
+	CreatedAt  time.Time `gorm:"autoCreateTime;not null"`
 }
